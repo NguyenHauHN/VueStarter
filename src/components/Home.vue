028683c65d4feb70dacc7a9390a9f0a9f57f00b2
haunguyen <nguyenhau1996mta@gmail.com>
2018-03-30 09:35:49 +0700

    todolist

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,96 +1,89 @@
 <template>
-  <div class="hello">
-    <Header v-if="me" :user="me.user"></Header>
-    <h1>Home</h1>
-    <button class="btn" @click="activeQuery = !activeQuery">activeQuery</button>
-    <div class="container" v-if="!me">
-      <div class="d-flex flex-row justify-content-center">
-        <div style="width: 360px;">
-          <form @submit="submit">
-            <div class="form-group">
-              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"
-                     v-model="vm.email">
+  <section class="todoapp mx-auto" style="width: 400px;">
+    <header class="header">
+      <h2>Todos</h2>
+      <input v-model="newTodo"
+             placeholder="What needs to be done?"
+             autocomplete="off"
+             autofocus
+             class="new-todo form-control input-lg"
+             @keyup.enter="addTodo()"
+      />
+    </header>
+    <section class="main mt-3" v-cloak style="min-height: 150px; border: 1px solid #eee;">
+      <div v-if="todos.length > 0">
+        <ul class="list-group">
+          <li class="list-group-item d-flex flex-row justify-content-between" v-for="(item, index) in todos">
+            <div>
+              <input type="checkbox" aria-label="Checkbox for following text input" v-model="item.active">
+              <span>{{item.value}}</span>
             </div>
-            <div class="form-group">
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"
-                     v-model="vm.password" @keyup.13="submit"
-              >
+            <div>
+              <button type="button" class="btn btn-block" @click="removeTodo(index)">x</button>
             </div>
-            <button class="btn btn-md btn-block" type="submit">Login</button>
-          </form>
-        </div>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div v-if="!$apollo.queries.users.loading && users">
-      <div v-for="item in users.results">
-        <h5>{{ item._id }}</h5>
-      </div>
-    </div>
-    <NestedView1 v-if="me"></NestedView1>
-    <NestedView2 v-if="me"></NestedView2>
-  </div>
+
+    </section>
+    <section class="footer mt-3">
+      <button type="button" class="btn btn-secondary">All</button>
+      <button type="button" class="btn btn-secondary" @click="filterActiveTodo()">Active</button>
+    </section>
+  </section>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import {QUERY} from "../graphql";
-import NestedView1 from '@/components/NestedView1'
-import NestedView2 from '@/components/NestedView2'
-import Header from '@/components/Header'
 
-export default {
-  name: 'Home',
-  created () {
-  },
-  mounted () {
-  },
-  data () {
-    return {
-      users: null,
-      activeQuery: true,
-      vm: {
-        email: 'bfontaine1@dell.com',
-        password: 'hello123123'
-      },
-      limit: 10,
-      offset: 0
-    }
-  },
-  methods: {
-    submit() {
-      console.log(this.vm)
-      let loginApi = 'https://auth.mathover.com:7999/auth/login'
-      this.$http.post(loginApi, this.vm).then(
-        (response) => {
-          this.$store.commit('updateLoggedInUser', response.data)
-        }
-      )
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'me'
-    ])
-  },
-  apollo: {
-    users: {
-      query: QUERY.users,
-      variables() {
-        return {
-          limit: this.limit,
-          offset: this.offset
-        }
-      },
-      skip(){
-        return this.activeQuery
+  export default {
+    name: 'Home',
+    created() {
+    },
+    mounted() {
+    },
+    data() {
+      return {
+        todos: [],
+        newTodo: ''
       }
-    }
-  },
-  components: {
-    NestedView1,
-    NestedView2,
-    Header
+    },
+    methods: {
+      addTodo() {
+        let value = this.newTodo && this.newTodo.trim()
+
+        if (!value) return
+
+        this.todos.push({
+          value: value,
+          active: false
+        });
+        this.newTodo = "";
+      },
+
+      filterActiveTodo() {
+        this.todos = this.todos.filter(item => {
+          return item.active;
+        })
+      },
+
+      removeTodo(index){
+        console.log(index);
+
+        this.todos.splice(index, 1)
+        console.log(this.todos)
+      }
+    },
+    computed: {}
   }
-}
 </script>
+
+<style scoped>
+  [v-cloak] {
+    display: none;
+  }
+
+  li.list-group-item {
+    text-align: left;
+  }
+</style>
 
